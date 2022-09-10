1f68d2d73420c17b320d2d022dab4f4c937b56c5
Ivan <ravic.ivan88@gmail.com>
2022-09-10 18:20:03 +0200

    Problem whit mui

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Divider, Box, Grid } from "@mui/material";
+import { Divider, Box, Grid, StyledEngineProvider } from "@mui/material";
 
 import Typography from "@mui/material/Typography";
 
@@ -8,53 +8,62 @@ import SavingsOutlinedIcon from "@mui/icons-material/SavingsOutlined";
 import AlarmOnOutlinedIcon from "@mui/icons-material/AlarmAddOutlined";
 import PaymentsOutlinedIcon from "@mui/icons-material/PaymentOutlined";
 import styles from "../styles/Featured.module.css";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 const Featured = () => {
   return (
-    <Box m={15} sx={{ flexGrow: 1 }} className={styles.card} elevation={1}>
-      <Grid mt={15} alignItems="center" justify="center" spacing={2} container>
-        <Grid align="center" item xs={12} md={3} lg={3}>
-          <div>
-            <LocalShippingOutlinedIcon className={styles.icons} />
-          </div>
-          <Divider />
-          <div>
-            <Typography variant="subtitle1">Fast delivery</Typography>
-            <Typography variant="body2">Start from $20</Typography>
-          </div>
+    <StyledEngineProvider inkectFirst>
+      <Box m={15} sx={{ flexGrow: 1 }} className={styles.card} elevation={1}>
+        <Grid
+          mt={15}
+          alignItems="center"
+          justify="center"
+          spacing={2}
+          container
+        >
+          <Grid align="center" item xs={12} md={3} lg={3}>
+            <div>
+              <LocalShippingOutlinedIcon className={styles.icons} />
+            </div>
+            <Divider />
+            <div>
+              <Typography variant="subtitle1">Fast delivery</Typography>
+              <Typography variant="body2">Start from $20</Typography>
+            </div>
+          </Grid>
+          <Grid align="center" item xs={12} md={3} lg={3}>
+            <div>
+              <SavingsOutlinedIcon className={styles.icons} />
+            </div>
+            <Divider />
+            <div>
+              <Typography variant="subtitle1">Money Guarantee</Typography>
+              <Typography variant="body2">7 Days Back</Typography>
+            </div>
+          </Grid>
+          <Grid align="center" item xs={12} md={3} lg={3}>
+            <div>
+              <AlarmOnOutlinedIcon className={styles.icons} />
+            </div>
+            <Divider />
+            <div>
+              <Typography variant="subtitle1">90 Days</Typography>
+              <Typography variant="body2">For free return</Typography>
+            </div>
+          </Grid>
+          <Grid align="center" item xs={12} md={3} lg={3}>
+            <div>
+              <PaymentsOutlinedIcon className={styles.icons} />
+            </div>
+            <Divider />
+            <div>
+              <Typography variant="subtitle1">Payment</Typography>
+              <Typography variant="body2">Secure payment</Typography>
+            </div>
+          </Grid>
         </Grid>
-        <Grid align="center" item xs={12} md={3} lg={3}>
-          <div>
-            <SavingsOutlinedIcon className={styles.icons} />
-          </div>
-          <Divider />
-          <div>
-            <Typography variant="subtitle1">Money Guarantee</Typography>
-            <Typography variant="body2">7 Days Back</Typography>
-          </div>
-        </Grid>
-        <Grid align="center" item xs={12} md={3} lg={3}>
-          <div>
-            <AlarmOnOutlinedIcon className={styles.icons} />
-          </div>
-          <Divider />
-          <div>
-            <Typography variant="subtitle1">90 Days</Typography>
-            <Typography variant="body2">For free return</Typography>
-          </div>
-        </Grid>
-        <Grid align="center" item xs={12} md={3} lg={3}>
-          <div>
-            <PaymentsOutlinedIcon className={styles.icons} />
-          </div>
-          <Divider />
-          <div>
-            <Typography variant="subtitle1">Payment</Typography>
-            <Typography variant="body2">Secure payment</Typography>
-          </div>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </StyledEngineProvider>
   );
 };
 
