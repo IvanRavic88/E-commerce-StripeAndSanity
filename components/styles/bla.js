019e1ff0b9849b435e354acff308519833fa1c9b
Ivan <ravic.ivan88@gmail.com>
2022-09-12 13:03:56 +0200

    file name changes

--- components/styles/bla.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export default makeStyles((theme) => ({
-  card: {
-    marginTop: "2rem",
-  },
-  icons: { padding: "1rem", color: "#2b3445", height: "5rem", width: "5rem" },
-}));
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  future: { webpack: true },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
