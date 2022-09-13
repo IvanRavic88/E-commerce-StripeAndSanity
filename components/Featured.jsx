75a0b9dc6f6700a9e96c81d74870e70b07ac9efd
Ivan <ravic.ivan88@gmail.com>
2022-09-13 14:01:45 +0200

    Final styling MUI

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -12,48 +12,77 @@ import makeStyles from "./styles/featured.js";
 
 const Featured = () => {
   const styles = makeStyles();
+
   return (
     <Box m={15} className={styles.card} elevation={1}>
       <Grid mt={15} alignItems="center" justify="center" spacing={2} container>
-        <Grid align="center" item xs={12} md={3} lg={3}>
+        <Grid
+          className={styles.featuredIcons}
+          align="center"
+          item
+          xs={12}
+          md={3}
+          lg={3}
+        >
           <div>
             <LocalShippingOutlinedIcon className={styles.icons} />
           </div>
           <Divider />
-          <div>
+          <Box>
             <Typography variant="subtitle1">Fast delivery</Typography>
             <Typography variant="body2">Start from $20</Typography>
-          </div>
+          </Box>
         </Grid>
-        <Grid align="center" item xs={12} md={3} lg={3}>
+        <Grid
+          className={styles.featuredIcons}
+          align="center"
+          item
+          xs={12}
+          md={3}
+          lg={3}
+        >
           <div>
             <SavingsOutlinedIcon className={styles.icons} />
           </div>
           <Divider />
-          <div>
+          <Box>
             <Typography variant="subtitle1">Money Guarantee</Typography>
             <Typography variant="body2">7 Days Back</Typography>
-          </div>
+          </Box>
         </Grid>
-        <Grid align="center" item xs={12} md={3} lg={3}>
+        <Grid
+          className={styles.featuredIcons}
+          align="center"
+          item
+          xs={12}
+          md={3}
+          lg={3}
+        >
           <div>
             <AlarmOnOutlinedIcon className={styles.icons} />
           </div>
           <Divider />
-          <div>
+          <Box>
             <Typography variant="subtitle1">90 Days</Typography>
             <Typography variant="body2">For free return</Typography>
-          </div>
+          </Box>
         </Grid>
-        <Grid align="center" item xs={12} md={3} lg={3}>
+        <Grid
+          className={styles.featuredIcons}
+          align="center"
+          item
+          xs={12}
+          md={3}
+          lg={3}
+        >
           <div>
             <PaymentsOutlinedIcon className={styles.icons} />
           </div>
           <Divider />
-          <div>
+          <Box>
             <Typography variant="subtitle1">Payment</Typography>
             <Typography variant="body2">Secure payment</Typography>
-          </div>
+          </Box>
         </Grid>
       </Grid>
     </Box>
--- components/styles/featured.js
+++ components/styles/featured.js
@@ -1,8 +1,16 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core";
 
 export default makeStyles((theme) => ({
   card: {
     marginTop: "2rem",
   },
   icons: { padding: "1rem", color: "#2b3445", height: "5rem", width: "5rem" },
+  [theme.breakpoints.down("sm")]: {
+    featuredIcons: {
+      "&&": {
+        paddingLeft: "0px",
+        maxWidth: "none",
+      },
+    },
+  },
 }));
